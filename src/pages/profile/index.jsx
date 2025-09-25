d87625776a3cc99b8459343d93db6ab3172dc527
jeremyda173 <jeremygithud173@gmail.com>
2025-09-24 22:13:21 -0400

    add profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,18 +1,14 @@
-import styles from "./profile.module.css";
+import styles from "./styles.module.css";
 
 function Profile({ user }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <div className={styles.avatar}>
-          {user.nombre.charAt(0).toUpperCase()}
-        </div>
-        
-        <h2 className={styles.name}>{user.nombre}</h2>
-        <p className={styles.email}>{user.email}</p>
-
-        <button className={styles.editButton}>Editar perfil</button>
+    <div className={styles.profileCard}>
+      <div className={styles.avatar}>
+        {user.nombre.charAt(0).toUpperCase()}
       </div>
+
+      <h2 className={styles.name}>{user.nombre}</h2>
+      <p className={styles.email}>{user.email}</p>
     </div>
   );
 }
